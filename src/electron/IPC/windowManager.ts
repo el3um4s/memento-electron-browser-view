d42b80283feb33d65b0028cb58c78711463792b5
Samuele de Tomasi <el3um4s@gmail.com>
2022-01-19 14:30:43 +0100

    open link in a new window
    Per il momento apre i link in una nuova finestra

--- src/electron/IPC/windowManager.ts
+++ src/electron/IPC/windowManager.ts
@@ -2,6 +2,11 @@ import { SendChannels } from "./General/channelsInterface";
 import IPC from "./General/IPC";
 import { BrowserWindow } from "electron";
 
+import CustomWindow from "../customWindow";
+import path from "path";
+
+import windowControls from "./windowControls";
+
 const nameAPI = "windowManager";
 
 // to Main
@@ -22,10 +27,24 @@ export default windowManager;
 
 // Enter here the functions for ElectronJS
 
-function openInNewWindow(
+async function openInNewWindow(
   mainWindow: BrowserWindow,
   event: Electron.IpcMainEvent,
   message: any
 ) {
   console.log(message);
+  await createMainWindow(message.link);
+}
+
+async function createMainWindow(destination: string) {
+  let customWindow: CustomWindow;
+  const settings = {
+    title: "MEMENTO - Svelte, Tailwind, Electron & TypeScript",
+  };
+  customWindow = new CustomWindow(settings);
+  const urlPage = destination;
+  customWindow.createWindow(urlPage);
+
+  await customWindow.setIpcMain([windowControls]);
+  return customWindow;
 }
