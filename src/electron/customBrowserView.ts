4b4da55e9e8d78d678f0dbed411f110c3c46fccb
Samuele de Tomasi <el3um4s@gmail.com>
2022-01-20 15:55:29 +0100

    Delete customBrowserView.ts

--- src/electron/customBrowserView.ts
+++
@@ -1,8 +0,0 @@
-import { BrowserView } from "electron";
-import path from "path";
-
-export const view = new BrowserView({
-  webPreferences: {
-    preload: path.join(__dirname, "preload.js"), // needs full path
-  },
-});
